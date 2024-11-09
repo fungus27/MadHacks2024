ce15c89ecf1c3420178d06e979499d7abd81dd87
th3oll <larrieuxtheo@gmail.com>
2024-11-09 17:11:08 -0600

    Unite websearch with llm

--- backend/server.ts
+++ backend/server.ts
@@ -12,7 +12,7 @@ const PORT = 8888;
 app.use(express.json())
 
 app.post('/tts', sendTTSResponse);
-app.post('/llm', sendLLMResponse);
+app.get('/llm/:prompt', sendLLMResponse);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- backend/utils/llm/llm.ts
+++ backend/utils/llm/llm.ts
@@ -1,14 +1,32 @@
 import { GoogleGenerativeAI } from '@google/generative-ai'
+import websearch from '../websearch/websearch';
+import dotenv from 'dotenv'; 
+
+dotenv.config()
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-export default async function sendLLMResponse(req : Request, res : Response) {
-    if (!req.body.hasOwnProperty('prompt'))
-        throw new Error("Invalid request");
+export default async function sendLLMResponse(req, res) {
+    const prompt = req.params.prompt;
 
-    const result = await model.generateContent(req.body['prompt']);
+    if (!prompt) {
+        console.log("Invalid request");
+        return res.status(400).send("Invalid request");
+    }
+
+    const searchResults = await websearch(prompt);
+
+    const template = `
+    You are a search result summarizer, given the QUERY below and the output of a search result, please give the user the information they want in at most three sentences. Your only job is to summarize as if you were reporting the news. Be objective, consistent, and include details.
+
+    QUERY: "${prompt}"
+    
+    RESULTS: "${searchResults}"
+    `;
+
+    const result = await model.generateContent(searchResults);
     
     res.json({
         data: result
