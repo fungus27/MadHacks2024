7c1beef37dbb4fbe209ea29070b3b4cd6dfdb7d8
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2024-11-09 22:48:58 -0600

    changed backend to serve audio files

--- backend/utils/tts/tts.ts
+++ backend/utils/tts/tts.ts
@@ -2,10 +2,16 @@ import * as textToSpeech from '@google-cloud/text-to-speech';
 import { Request, Response } from 'express';
 import { Buffer } from "buffer";
 import dotenv from 'dotenv'; 
+import * as tmp from 'tmp';
+import * as util from 'util';
+import * as fs from 'fs';
 
 dotenv.config()
 
+tmp.setGracefulCleanup();
+
 const client = new textToSpeech.TextToSpeechClient(); // uses ADC
+
 
 export async function getTextAudio(text: string,
                             languageCode : string = 'en-US',
@@ -17,20 +23,27 @@ export async function getTextAudio(text: string,
     audioConfig: { audioEncoding: audioEncoding },
   };
 
+  //var files = fs.readdirSync('C:/tmp').filter(fn => fn.endsWith('.csv'))
+
   //@ts-ignore
   const [response] = await client.synthesizeSpeech(request);
-  return response.audioContent.toString('base64');
+  const writeFile = util.promisify(fs.writeFile);
+  const tmpFile = tmp.fileSync({tmpdir: ".", postfix: ".mp3"});
+  console.log(tmpFile.name);
+
+  await writeFile(tmpFile.name, response.audioContent, 'binary');
+
+  return tmpFile.name;
 }
 
 export default async function sendTTSResponse(req : Request, res : Response) {
-    if (!req.body.hasOwnProperty('text'))
-        throw new Error("Invalid request");
+    const text = req.params.text;
 
-    const text = req.body['text']; 
+    const audioFileName = await getTextAudio(text);
 
-    const audioData = await getTextAudio(text);
+    /*     res.json({
+        audioFile: audioFileName
+    });  */
 
-    res.json({
-        audio: audioData
-    }); 
+   res.sendFile(audioFileName);
 }
