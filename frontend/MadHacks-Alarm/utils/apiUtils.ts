7105f20065d4959929fc6c1f76abdbbdd9a9d597
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2024-11-09 19:20:22 -0600

    implement frontend backend communication

--- frontend/MadHacks-Alarm/utils/apiUtils.ts
+++ frontend/MadHacks-Alarm/utils/apiUtils.ts
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
-    });
+    baseURL: 'http://localhost:8888',
+});
 
-export const getTTS = async (text: string, queryToLookup: string) => {
-    return await api.get('/tts', {
-        params: {
-            text,
-            queryToLookup
-        }
+export const getTTS = async (text: string) => {
+    return await api.post('/tts', {
+        text: text
     });
+}
+
+export const getTTSfromQuery = async (query: string) => {
+    return await api.get('/llm/' + query);
 }
